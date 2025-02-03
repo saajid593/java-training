2e58cf18be01f170042a4f84d38d38f81177ba66
Mohamed Saajid <mohamedsaajid0@gmail.com>
2025-02-03 15:42:17 +0530

    my first commit

---
+++ Hello.java
@@ -0,0 +1,14 @@
+ class hello{
+
+     public static void main(String[] args) {
+        
+       
+       
+       int leftshift = 6<<2;
+       System.out.println(6<<2);
+       System.out.println(16>>3);
+     }
+     
+
+
+}
---
+++ OddEvenBit.java
@@ -0,0 +1,23 @@
+public class OddEvenBit {
+
+    public static void main(String args []){
+
+        int a = 7;
+
+        int check = a&1;
+    
+
+    
+    if(check==0){
+
+        System.out.println("Odd");
+    }else{
+
+        System.out.println("Even");
+    
+    }
+}
+}
+
+    
+
