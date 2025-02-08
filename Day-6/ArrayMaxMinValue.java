f813b14732ea501f788d2e8abbac5a04adc15ce7
Mohamed Saajid <mohamedsaajid0@gmail.com>
2025-02-08 11:36:49 +0530

    Day-6

---
+++ Day-6/ArrayMaxMinValue.java
@@ -0,0 +1,34 @@
+public class ArrayMaxMinValue{
+    public static void main(String[] args) {
+        
+        int a[]={4,8,3,6,1};
+        int first=Integer.MAX_VALUE;
+        int second=Integer.MAX_VALUE;
+        int third=Integer.MAX_VALUE;
+
+        for(int i =0;i<a.length;i++)
+        {
+            if(first>a[i]){
+                third=second;
+                second = first;
+                
+                first = a[i];
+            }
+            else if(second>a[i]){
+                third = second;
+                   second=a[i];
+            }
+            else if(third>a[i]){
+                third=a[i];
+            }
+            
+        }
+        System.out.println(first+" "+second+" "+third);
+               
+        
+               
+
+
+    }
+    
+}
---
+++ Day-6/FrequencyArray.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+public class FrequencyArray {
+    public static void main(String args []){
+        int a[]={1,2,3,4,5,2,5,4};
+        int freq[]=new int[a.length];
+        for(int i = 0;i<a.length;i++){
+
+            int no=a[i];
+            int count=1;
+            for(int j=i+1;j<a.length;j++){
+            
+                if(no==a[j]){
+                    count++;
+                    freq[j]=-1;
+                }
+            
+            if(freq[i]!=-1){
+            
+                freq[i]= count;
+            
+            
+                }
+            
+            for(i=0;i<a.length;i++){
+                if(freq[i]!=1){
+                    System.out.println(a[j]+"comes"+freq+"times");
+                }
+
+            }
+        }
+           
+    }  
+}
+}
+    
+
